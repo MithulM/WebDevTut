09504147514aedf6b897f8c9f9f12cb29833e8c8
Mithul Manivannan <mithulmanimala@gmail.com>
2021-06-11 13:29:36 -0500

    Added canavas and 100 random lines

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    let canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
+
+    for(let i = 0; i < 100; i += 1) {
+        context.beginPath();
+        context.moveTo(Math.random() * width, Math.random() * height);
+        context.lineTo(Math.random() * width, Math.random() * height);
+        context.stroke()
+    }
+
+}
