8d6f12346af8d5218a0d668022565aa8d56b6b05
Mithul Manivannan <mithulmanimala@gmail.com>
2021-06-12 13:09:24 -0500

    added Circle class

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-window.onload = function() {
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+window.onresize = window.onload = function() {
     let canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
     width = canvas.width = window.innerWidth;
     height = canvas.height = window.innerHeight;
-
     for(let i = 0; i < 100; i += 1) {
         context.beginPath();
         context.moveTo(Math.random() * width, Math.random() * height);
         context.lineTo(Math.random() * width, Math.random() * height);
-        context.stroke()
+        context.stroke();
     }
+};
+const circle = new Circle(10);
 
+for (let key in circle) {
+    console.log(typeof key)
 }
+window.resizeTo(100, 100);
+window.focus();
+
